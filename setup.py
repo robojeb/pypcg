c1145547f6a11d8d8e4afb3065d8814f90cc7769
Jeb Brooks <robojeb@gmail.com>
2015-04-01 16:59:00 -0700

    working on getting distribution to work correctly

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 # coding=utf-8
 
-from distutils.core import setup
-import pypcg
+from distutils.command.build import build
+from setuptools import setup
+from setuptools.command.install import install
+
+
+def get_ext_modules():
+    import pypcg
+    return [pypcg.ffi.verifier.get_extension()]
+
+
+class CFFIBuild(build):
+    def finalize_options(self):
+        self.distribution.ext_modules = get_ext_modules()
+        build.finalize_options(self)
+
+
+class CFFIInstall(install):
+    def finalize_options(self):
+        self.distribution.ext_modules = get_ext_modules()
+        install.finalize_options(self)
 
 setup(name='pypcg',
-      version=pypcg.VERSION,
+      version='0.0.1b',
       license='MIT',
 
       author="Jeb Brooks",
@@ -13,14 +31,19 @@ setup(name='pypcg',
 
       zip_safe=False,
 
-      ext_package='pypcg',
-      ext_modules=[pypcg.ffi.verifier.get_extension()],
+      # ext_package='pypcg',
+      # ext_modules=[pypcg.ffi.verifier.get_extension()],
 
       packages=['pypcg'],
       package_dir={'pypcg': 'pypcg'},
       package_data={'pypcg': ['pcg_basic/pcg_basic.*']},
 
       install_requires=['cffi'],
-      setup_required=['cffi']
+      setup_requires=['cffi'],
+
+      cmdclass={
+        "build": CFFIBuild,
+        "install": CFFIInstall,
+      }
 
       )
